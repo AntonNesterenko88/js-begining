3043474dad6d36e091c3b4a9793f3d2babdc36bb
AntonNesterenko88 <anton.nesterenko.44@gmail.com>
2025-03-15 21:36:44 +0300

    first js tryout

---
+++ prompt.js
@@ -0,0 +1,7 @@
+let userName = prompt ("Как твое имя?")  
+alert ( `Тебя зовут ${userName}?`)
+console.log ("Пользователь ввел имя: " + userName)
+
+let userSurName = prompt ("Как твоя фамилия?")
+alert ( "Твоя фамилия " + userSurName )
+console.log ("Пользователь ввел фамилию: " + userSurName)
