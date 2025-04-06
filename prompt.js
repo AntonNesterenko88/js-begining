e3fba371027bd6f14090e44d6948e475f86fad64
AntonNesterenko88 <anton.nesterenko.44@gmail.com>
2025-04-06 10:41:39 +0300

    first commit

--- prompt.js
+++ prompt.js
@@ -1,7 +1,52 @@
-let userName = prompt ("Как твое имя?")  
-alert ( `Тебя зовут ${userName}?`)
-console.log ("Пользователь ввел имя: " + userName)
+let legalAge = prompt ( "Сколько Вам лет?")
 
-let userSurName = prompt ("Как твоя фамилия?")
-alert ( "Твоя фамилия " + userSurName )
-console.log ("Пользователь ввел фамилию: " + userSurName)
+if (NaN) {
+    alert ("Вы совершеннолетний!");
+}
+
+     else {
+        if (legalAge >= 18) {
+        alert("Вы совершеннолетний");
+        } else {
+        alert("Вы несовершеннолетний. Покиньте страницу!");
+    }
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    alert(i);
+  }
+
+  let dayOfWeek = new Date().getDay(); 
+
+switch (dayOfWeek) {
+  case 0:
+    alert ("Сегодня Понедельник");
+    break;
+
+  case 1:
+    alert ("Сегодня Вторник");
+    break;
+
+  case 2:
+    alert ("Сегодня Среда");
+    break;
+
+  case 3:
+    alert ("Сегодня Четверг");
+    break;
+
+  case 4:
+    alert ("Сегодня Пятница");
+    break;
+
+  case 5:
+    alert ("Сегодня Суббота");
+    break;
+
+  case 6:
+    alert ("Сегодня Воскресенье");
+    break;
+    
+  default:
+    alert ("Ошибка: Некорректный день недели.");
+}
