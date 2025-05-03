c95fa8654ddbe53b87f0d83f5857f703d9d5a122
AntonNesterenko88 <anton.nesterenko.44@gmail.com>
2025-05-03 21:09:03 +0300

    functions

---
+++ js-function/function.js
@@ -0,0 +1,21 @@
+function multiply (a, b) {
+  return a * b ;
+}
+
+let result = multiply (5, 3); {
+  console.log(result);
+}
+
+function isAdult(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+const getSquare = (number) => number * number;
+
+
+  console.log (getSquare(5));
